4bc56f7c3bc21dd11374b244d79fc50e36fa7f76
nzirahsolutions <nzirahsolutions@gmail.com>
2026-04-10 17:29:31 +0300

    prod vs dev environment logic

--- mtclient/src/Context/urls.js
+++ mtclient/src/Context/urls.js
@@ -1,3 +1,4 @@
-// export const currentServer="http://localhost:3500";
-export const currentServer="https://mesatab.onrender.com";
+const host=import.meta.env.VITE_NODE_ENV;
+export const currentServer=host==='development'? "http://localhost:3500":"https://mesatab.onrender.com";
+// export const currentServer="https://mesatab.onrender.com";
 //export const googleClientId="30599681861-i4urp3d6ovinjosaepd8p5gmtmb7fe30.apps.googleusercontent.com";
--- mtserver/src/db/db.ts
+++ mtserver/src/db/db.ts
@@ -3,5 +3,6 @@ import postgres from 'postgres';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 
-const client = postgres(process.env.CLOUD_DATABASE_URL!, { prepare: false });
+const host= process.env.NODE_ENV==='development'? process.env.LOCAL_DATABASE_URL : process.env.CLOUD_DATABASE_URL;
+const client = postgres(host!, { prepare: false });
 export const db = drizzle(client, { schema, logger: false});//logger true ensures every change in our database in logged in our console
